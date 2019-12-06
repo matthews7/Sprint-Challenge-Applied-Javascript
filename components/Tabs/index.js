23c70404581a94041c56f3250178ca7f3a28f3a5
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-12-06 09:37:13 -0700

    Tabs renedering

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,25 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+    .get(' https://lambda-times-backend.herokuapp.com/topics')
+    .then( response => {
+        response.data.topics.forEach( response => {
+            const newTab = Tab(response);
+            entryPoint.append(newTab);
+        })
+    })
+    .catch(error => {
+        console.log("The data was not returned", error);
+      });
+
+
+    function Tab (value){
+        const tabTopic = document.createElement('div');
+        tabTopic.classList.add('tab');
+        tabTopic.textContent = value;
+        return tabTopic
+    }
+
+    const entryPoint = document.querySelector('.topics')
